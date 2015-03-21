fd4421b85e4b6aada1658f8e4fe830fa5c4907de
CHENG ZENG <zengc@rose-hulman.edu>
2015-03-20 20:38:30 -0400

    Lab2

--- ExpediaTest/FlightTest.cs
+++ ExpediaTest/FlightTest.cs
@@ -8,5 +8,51 @@ namespace ExpediaTest
 	public class FlightTest
 	{
 		//TODO Task 7 items go here
+        private readonly DateTime StartDate = new DateTime(2009, 11, 1);
+        private readonly DateTime OneDayEndDate = new DateTime(2009, 11, 2);
+        private readonly DateTime TwoDayEndDate = new DateTime(2009, 11, 3);
+        private readonly DateTime TenDayEndDate = new DateTime(2009, 11, 11);
+
+
+
+        [Test()]
+        public void TestThatFlightInitializes()
+        {
+            var target = new Flight(StartDate, OneDayEndDate, 0);
+            Assert.IsNotNull(target);
+        }
+
+        [Test()]
+        public void TestThatFlightHasCorrectBasePriceForOneDayStay()
+        {
+            var target = new Flight(StartDate, OneDayEndDate, 0);
+            Assert.AreEqual(220, target.getBasePrice());
+        }
+        [Test()]
+        public void TestThatHotelHasCorrectBasePriceForTwoDayStay()
+        {
+            var target = new Flight(StartDate, TwoDayEndDate, 0);
+            Assert.AreEqual(240, target.getBasePrice());
+        }
+        [Test()]
+        public void TestThatHotelHasCorrectBasePriceForTenDaysStay()
+        {
+            var target = new Flight(StartDate, TenDayEndDate, 0);
+            Assert.AreEqual(400, target.getBasePrice());
+        }
+
+        [Test()]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void TestThatHotelThrowsOnNegativeMiles()
+        {
+            new Flight(StartDate, TenDayEndDate, -1);
+        }
+
+        [Test()]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void TestThatHotelThrowsOnEndDateBeforeStartDate()
+        {
+            new Flight(TenDayEndDate, StartDate, 0);
+        }
 	}
 }
